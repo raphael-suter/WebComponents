8f247ea1161c76a6738705dbebf4c1ede435f5f1
Raphael <raphael.suter@axa.ch>
2020-05-05 13:26:27 +0200

    Implement dialog

--- src/modal.js
+++ src/modal.js
@@ -1,6 +1,54 @@
-import { html, LitElement } from 'lit-element';
+import { css, html, LitElement } from 'lit-element';
 
 class Modal extends LitElement {
+  static get styles() {
+    return css`
+      .background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background: rgba(0, 0, 0, 0.75);
+        display: none;
+      }
+
+      :host([opened]) .background {
+        display: block;
+      }
+
+      .modal {
+        width: 50%;
+        margin: 100px auto;
+        background: white;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+      }
+
+      ::slotted(h1) {
+        margin: 0;
+        padding: 1rem;
+        border-bottom: 1px solid #ccc;
+      }
+
+      .main {
+        margin: 0 1rem;
+      }
+
+      .actions {
+        display: flex;
+        padding: 1rem;
+        justify-content: end;
+        border-top: 1px solid #ccc;
+      }
+
+      .actions button {
+        margin-left: 1rem;
+      }
+    `;
+  }
+
   render() {
     return html`
       <div class='background'>
@@ -12,12 +60,25 @@ class Modal extends LitElement {
             <slot></slot>
           </section>
           <section class='actions'>
-            <button id='cancel'>cancel</button>
-            <button id='ok'>ok</button>
+            <button @click="${this.hide}">cancel</button>
+            <button @click="${this.confirm}">ok</button>
           </section>
         </div>
       </div>
     `;
+  }
+
+  open() {
+    this.setAttribute('opened', '');
+  }
+
+  hide() {
+    this.removeAttribute('opened');
+  }
+
+  confirm() {
+    this.hide();
+    this.dispatchEvent(new Event('success'));
   }
 }
 
