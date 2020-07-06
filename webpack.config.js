e1e03ced67a330c07d7ac737e756a98db046b4a7
Raphael <raphael.suter@axa.ch>
2020-07-06 13:45:42 +0200

    Modify dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/components/index.js',
+  entry: "./src/components/index.js",
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   output: {
-    filename: 'bundle.[hash].js',
-    path: path.resolve(__dirname, 'build')
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "build"),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
-}
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    host: "192.168.178.52",
+    port: 8080,
+    disableHostCheck: true,
+  },
+};
